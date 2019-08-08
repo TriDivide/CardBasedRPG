1180f4a515a957f4ceb15ebbfaf277cef3d8c73c
TriDivide <liamsmitha@gmail.com>
2019-08-08 14:18:47 +0100

    - started work on the discard pile

---
+++ ArcaneCardGame/Assets/Scripts/U.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U {
+
+    public static int RandomNumber(int min, int max) {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
+    }
+}
---
+++ ArcaneCardGame/Assets/Scripts/Zones/DiscardZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DiscardZone : DropZone {
+
+
+    private void Start() {
+        this.cardTypeAccepted = null;
+    }
+
+    public new void OnDrop(PointerEventData eventData) {
+        Card d = eventData.pointerDrag.GetComponent<Card>();
+
+        bool snap = false;
+        if (d != null) {
+            if (cardTypeAccepted == null) {
+                snap = true;
+            } else {
+                snap = (cardTypeAccepted == d.cardType);
+            }
+        }
+
+        if (snap) {
+            d.parentToReturn = this.transform;
+        }
+
+        d.transform.position = this.transform.position;
+        Quaternion rotation = d.transform.rotation;
+        d.transform.Rotate(rotation.x, rotation.y, U.RandomNumber(-20, 20));
+    }
+}
